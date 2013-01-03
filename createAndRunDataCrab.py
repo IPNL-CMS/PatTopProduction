b9ccab6c818065ba2d59c7e865a264ef250fc3c4
Sébastien Brochet <s.brochet@ipnl.in2p3.fr>
2013-01-03 18:16:08 +0100

    added: Ecal recover dataset

--- createAndRunDataCrab.py
+++ createAndRunDataCrab.py
@@ -7,7 +7,7 @@ parser = OptionParser()
 parser.add_option("", "--run", action="store_true", dest="run", default=False, help="run crab")
 (options, args) = parser.parse_args()
 
-global_json = "Cert_190456-206940_8TeV_PromptReco_Collisions12_JSON.txt"
+global_json = "Cert_190456-208686_8TeV_PromptReco_Collisions12_JSON.txt"
 
 datasets = [
     ["/MuHad/Run2012A-13Jul2012-v1/AOD", "MuHad_Run2012A-13Jul2012", "Cert_190456-196531_8TeV_13Jul2012ReReco_Collisions12_JSON_v2.txt", "FT_53_V6_AN3", [190456, 193621]],
@@ -15,14 +15,16 @@ datasets = [
     ["/SingleMu/Run2012B-TOPMuPlusJets-13Jul2012-v1/AOD", "SingleMu_Run2012B-TOPMuPlusJets-13Jul2012", "Cert_190456-196531_8TeV_13Jul2012ReReco_Collisions12_JSON_v2.txt", "FT_53_V6_AN3", [193833, 196531]],
     ["/SingleMu/Run2012C-TOPMuPlusJets-24Aug2012-v1/AOD", "SingleMu_Run2012C-TOPMuPlusJets-24Aug2012", "Cert_198022-198523_8TeV_24Aug2012ReReco_Collisions12_JSON.txt", "FT_53_V10_AN3", [198022, 198913]],
     ["/SingleMu/Run2012C-TOPMuPlusJets-PromptSkim-v3/AOD", "SingleMu_Run2012C-TOPMuPlusJets-PromptSkim", "", "GR_P_V41_AN3", [198934, 203746]],
-    ["/SingleMu/Run2012D-TOPMuPlusJets-PromptSkim-v1/AOD", "SingleMu_Run2012D-TOPMuPlusJets-PromptSkim", "", "GR_P_V42_AN3", []],
+    ["/SingleMu/Run2012C-EcalRecover_11Dec2012-v1/AOD", "SingleMu_Run2012C-EcalRecover_11Dec2012", "Cert_201191-201191_8TeV_11Dec2012ReReco-recover_Collisions12_JSON.txt", "FT_P_V42C_AN3", [201191, 201191]],
+    ["/SingleMu/Run2012D-TOPMuPlusJets-PromptSkim-v1/AOD", "SingleMu_Run2012D-TOPMuPlusJets-PromptSkim", "", "GR_P_V42_AN3", [203768, 208686]],
 
     ["/ElectronHad/Run2012A-13Jul2012-v1/AOD", "ElectronHad_Run2012A-13Jul2012", "Cert_190456-196531_8TeV_13Jul2012ReReco_Collisions12_JSON_v2.txt", "FT_53_V6_AN3", [190456, 193621]],
     ["/ElectronHad/Run2012A-recover-06Aug2012-v1/AOD", "ElectronHad_Run2012A-recover-06Aug2012", "Cert_190782-190949_8TeV_06Aug2012ReReco_Collisions12_JSON.txt", "FT_53_V6C_AN3", [190782, 190949]],
     ["/SingleElectron/Run2012B-TOPElePlusJets-13Jul2012-v1/AOD", "SingleElectron_Run2012B-TOPElePlusJets-13Jul2012", "Cert_190456-196531_8TeV_13Jul2012ReReco_Collisions12_JSON_v2.txt", "FT_53_V6_AN3", [193833, 196531]],
     ["/SingleElectron/Run2012C-TOPElePlusJets-24Aug2012-v1/AOD", "SingleElectron_Run2012C-TOPElePlusJets-24Aug2012", "Cert_198022-198523_8TeV_24Aug2012ReReco_Collisions12_JSON.txt", "FT_53_V10_AN3", [198022, 198913]],
     ["/SingleElectron/Run2012C-TOPElePlusJets-PromptSkim-v3/AOD", "SingleElectron_Run2012C-TOPElePlusJets-PromptSkim", "", "GR_P_V41_AN3", [198934, 203746]],
-    ["/SingleElectron/Run2012D-TOPElePlusJets-PromptSkim-v1/AOD", "SingleElectron_Run2012D-TOPElePlusJets-PromptSkim", "", "GR_P_V42_AN3", []]
+    ["/SingleElectron/Run2012C-EcalRecover_11Dec2012-v1/AOD", "SingleElectron_Run2012C-EcalRecover_11Dec2012", "Cert_201191-201191_8TeV_11Dec2012ReReco-recover_Collisions12_JSON.txt", "FT_P_V42C_AN3", [201191, 201191]],
+    ["/SingleElectron/Run2012D-TOPElePlusJets-PromptSkim-v1/AOD", "SingleElectron_Run2012D-TOPElePlusJets-PromptSkim", "", "GR_P_V42_AN3", [203768, 208686]]
 
     ]
 
