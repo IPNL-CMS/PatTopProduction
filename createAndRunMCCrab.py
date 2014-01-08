aef68d39a5f7daa9b50424bd3b094c46b58134a7
Sébastien Brochet <s.brochet@ipnl.in2p3.fr>
2014-01-08 11:20:19 +0100

    Added support for custom datasets (not published in global database)

--- createAndRunMCCrab.py
+++ createAndRunMCCrab.py
@@ -4,8 +4,13 @@ import os, datetime, pwd, re, json
 
 from optparse import OptionParser
 parser = OptionParser()
+parser.add_option("", "--create", action="store_true", dest="create", default=False, help="create crab task")
 parser.add_option("", "--run", action="store_true", dest="run", default=False, help="run crab")
+parser.add_option("", "--submit", action="store_true", dest="submit", default=False, help="submit crab jobs")
 (options, args) = parser.parse_args()
+
+if options.run:
+    options.create = True
 
 datasets = {}
 for file in args:
@@ -24,19 +29,44 @@ print("")
 
 for dataset_path, dataset_name in datasets.items():
 
-  dataset_globaltag = re.search('START\d{0,2}_V\d[A-Z]?', dataset_path).group(0)
+  gt = re.search('START\d{0,2}_V\d[A-Z]?', dataset_path)
+  dataset_globaltag = None
 
-  publish_name = "%s_%s_%s-v%d" % (dataset_name, dataset_globaltag, d, version)
+  if gt is not None:
+    dataset_globaltag = gt.group(0)
+
+  if dataset_globaltag is not None:
+    publish_name = "%s_%s_%s-v%d" % (dataset_name, dataset_globaltag, d, version)
+  else:
+    publish_name = "%s_%s-v%d" % (dataset_name, d, version)
+
   ui_working_dir = ("crab_MC_%s") % (dataset_name)
   output_file = "crab_MC_%s.cfg" % (dataset_name)
 
-  print("Creating config file for '%s'" % (dataset_path))
-  print("\tName: %s" % dataset_name)
-  print("\tPublishing name: %s" % publish_name)
-  print("")
+  if options.create:
+    print("Creating config file for '%s'" % (dataset_path))
+    print("\tName: %s" % dataset_name)
+    print("\tPublishing name: %s" % publish_name)
+    print("")
 
-  os.system("sed -e \"s#@datasetname@#%s#\" -e \"s#@uiworkingdir@#%s#g\" -e \"s#@publish_data_name@#%s#g\" -e \"s#@email@#%s#g\" crab_MC.cfg.template.ipnl > %s" % (dataset_path, ui_working_dir, publish_name, email, output_file))
+    if "/USER" in dataset_path:
+        dbs_url = "http://cmsdbsprod.cern.ch/cms_dbs_ph_analysis_02/servlet/DBSServlet"
+    else:
+        dbs_url = "http://cmsdbsprod.cern.ch/cms_dbs_prod_global/servlet/DBSServlet"
 
-  cmd = "crab -create -submit -cfg %s" % (output_file)
+    os.system("sed -e \"s#@datasetname@#%s#\" -e \"s#@uiworkingdir@#%s#g\" -e \"s#@publish_data_name@#%s#g\" -e \"s#@email@#%s#g\" -e \"s#@dbs_url@#%s#g\" crab_MC.cfg.template.ipnl > %s" % (dataset_path, ui_working_dir, publish_name, email, dbs_url, output_file))
+
+
   if options.run:
+    cmd = "crab -create -submit -cfg %s" % (output_file)
+    os.system(cmd)
+
+  if options.submit:
+    cmd = "crab -submit 1-500 -c %s" % ui_working_dir
+    os.system(cmd)
+
+    cmd = "crab -submit 500-1000 -c %s" % ui_working_dir
+    os.system(cmd)
+
+    cmd = "crab -submit 1000-1500 -c %s" % ui_working_dir
     os.system(cmd)
